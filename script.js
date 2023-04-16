0aabfe3b824b1294e155432ebe119ebfd8e4b5f3
JesseJarvisalo <96780400+JesseJarvisalo@users.noreply.github.com>
2023-04-16 15:02:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Näytä lisää, vähemmän nappulan toiminto
 var toggleButtons = document.querySelectorAll('.toggle-description');
 
 toggleButtons.forEach(function(button) {
@@ -18,17 +19,33 @@ toggleButtons.forEach(function(button) {
   });
 });
 
-// Smooth scroll to section on click
-const links = document.querySelectorAll('nav a');
+
+// scroll to section ja bottom border animation
+
+const navLinks = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll('section');
 const navHeight = document.querySelector('nav').offsetHeight;
 
-links.forEach(link => {
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const sectionClass = entry.target.getAttribute('class');
+      const navLink = document.querySelector(`nav a[href=".${sectionClass}"]`);
+      navLinks.forEach(link => link.classList.remove('active'));
+      navLink.classList.add('active');
+    }
+  });
+}, { rootMargin: '-50% 0px -50% 0px' });
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+navLinks.forEach(link => {
   link.addEventListener('click', function(event) {
     event.preventDefault();
-
     const target = document.querySelector(this.getAttribute('href'));
     const targetPosition = target.getBoundingClientRect().top + window.scrollY - navHeight;
-
     window.scrollTo({
       top: targetPosition,
       behavior: 'smooth'
@@ -36,4 +53,11 @@ links.forEach(link => {
   });
 });
 
+// contact bar animation
+const button = document.querySelector('.ota-yhteytta button');
+const contactBar = document.querySelector('.contact-bar');
+
+button.addEventListener('click', () => {
+  contactBar.classList.toggle('open');
+});
 
